b6c8a707d572c85b0f0449d8c74d859e65a0e5ca
Jakub <rolls-hugs.6u@icloud.com>
2025-11-08 00:25:28 +0000

    I have added a font property to the ticks object of the x-axis in main.js, setting its size to config.forecast.labels_font_size. This should now control the font size of the time labels in the hourly forecast axis.

--- src/main.js
+++ src/main.js
@@ -636,6 +636,9 @@ drawChart({ config, language, weather, forecastItems } = this) {
               maxRotation: 0,
               color: config.forecast.chart_datetime_color || textColor,
               padding: config.forecast.precipitation_type === 'rainfall' && config.forecast.show_probability && config.forecast.type !== 'hourly' ? 4 : 10,
+              font: {
+                size: config.forecast.labels_font_size,
+              },
               callback: function (value, index, values) {
                   var datetime = this.getLabelForValue(value);
                   var dateObj = new Date(datetime);
